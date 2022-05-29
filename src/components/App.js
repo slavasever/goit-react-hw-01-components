0cc8a103b20d872e50d06731d64ed48d8fa93e03
Viacheslav Severyn <sever_2008@ukr.net>
2022-05-29 12:33:52 +0300

    3_components_fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-// import Profile from 'components/Profile/Profile';
-// import user from '../user.json';
+import Profile from 'components/Profile/Profile';
+import user from '../user.json';
 import Statistics from 'components/Statistics/Statistics';
 import data from '../data.json';
 import Friendlist from 'components/FriendList/FriendList';
@@ -8,13 +8,13 @@ import friends from '../friends.json';
 const App = () => {
   return (
     <div>
-      {/* <Profile
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      /> */}
+      />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <Friendlist props={friends} />
