98cf2a71da71d2a4ba6705da5a4a93ea10491c38
Viacheslav Severyn <sever_2008@ukr.net>
2022-05-30 17:39:51 +0300

    done_v1.1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,7 +8,7 @@ function Profile(params) {
       <div className={s.description}>
         <img src={avatar} alt="User avatar" className={s.avatar} />
         <p className={s.name}>{username}</p>
-        <p className={s.tag}>{tag}</p>
+        <p className={s.tag}>@{tag}</p>
         <p className={s.location}>{location}</p>
       </div>
 
